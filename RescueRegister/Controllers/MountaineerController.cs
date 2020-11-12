ca03ee2cdf97d648af5492d79eec2219ceb12948
Gregoriy Milushev <burzakata@gmail.com>
2020-11-12 12:24:13 +0200

    First commit

---
+++ RescueRegister/Controllers/MountaineerController.cs
@@ -0,0 +1,105 @@
+﻿using RescueRegister.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace RescueRegister.Controllers
+{
+    public class MountaineerController : Controller
+    {
+        private readonly RescueRegisterDbContext context;
+
+        public MountaineerController(RescueRegisterDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            using (var db = new RescueRegisterDbContext())
+            {
+                var allMountaineers = db.Mountaineers.ToList();
+                return View(allMountaineers);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            // TODO: Implement me
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Mountaineer mountaineer)
+        {
+
+            using (var db = new RescueRegisterDbContext())
+            {
+                db.Mountaineers.Add(mountaineer);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (var db = new RescueRegisterDbContext())
+            {
+                var mountaineerToEdit = db.Mountaineers.FirstOrDefault(x => x.Id == id);
+                if (mountaineerToEdit == null)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                return View(mountaineerToEdit);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Mountaineer mountaineer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            using (var db = new RescueRegisterDbContext())
+            {
+                db.Mountaineers.Update(mountaineer);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            using (var db = new RescueRegisterDbContext())
+            {
+                var mountaineerToDelete = db.Mountaineers.FirstOrDefault(x => x.Id == id);
+                if (mountaineerToDelete == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View(mountaineerToDelete);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Mountaineer mountaineer)
+        {
+            if (mountaineer == null)
+            {
+                return RedirectToAction("Index");
+            }
+            using (var db = new RescueRegisterDbContext())
+            {
+                db.Mountaineers.Remove(mountaineer);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
